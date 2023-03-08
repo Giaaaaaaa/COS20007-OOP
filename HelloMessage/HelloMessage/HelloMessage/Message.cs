5be8bc5754fe694c469d0b103f750d463edfaa1a
wenjia <dwgia027@gmail.com>
2023-03-09 09:46:34 +1100

    helloMessage down week 1

---
+++ HelloMessage/HelloMessage/HelloMessage/Message.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace HelloMessage
+{
+    internal class Message
+    {
+        private string text;
+        public Message(string txt) 
+        {
+            text = txt;
+        }
+        public void Print()
+        {
+            Console.WriteLine(text);
+        }
+    }
+}
---
+++ HelloMessage/HelloMessage/HelloMessage/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloMessage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string myString;
+            myString = "abc";
+            Message myMessage;
+            myMessage = new Message(myString);
+            myMessage.Print();
+
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+
+        }
+    }
+}
